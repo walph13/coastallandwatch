d3fbba9061e4788db7373c68ab8061fc43843c10
walph13 <yellyhazee69@gmail.com>
2026-03-15 02:00:00 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,1 +1,14 @@
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // Default XAMPP password
+$dbname = "barangay_waste_reporting"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully"; 
+?>
